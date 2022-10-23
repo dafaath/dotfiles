75c87bfb127cfa05fa723bc67eed2457baae39bb
dapaath <dafaathaullah123@gmail.com>
2022-10-23 08:48:07 +0700

    new-update Sun Oct 23 08:48:07 WIB 2022

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -15,7 +15,6 @@ lvim.colorscheme = "tokyonight-storm"
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 
-
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["<C-l>"] = ":BufferLineCycleNext<CR>"
