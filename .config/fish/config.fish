736804a6143bd4a4c579773788eb46c363b2c5bf
dapaath <dafaathaullah123@gmail.com>
2022-12-12 00:10:08 +0700

    new-update Mon Dec 12 00:10:08 WIB 2022

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,6 +9,7 @@ set PATH ~/go/bin $PATH
 set PATH /opt/upscaler $PATH
 set PATH /Apps/butler $PATH
 set PATH /home/dafaath/Project/Analgor_Grading $PATH
+set PATH /home/dafaath/.fly/bin $PATH
 set PYTHONPATH /usr/bin/python
 set VISUAL lvim
 set EDITOR lvim
