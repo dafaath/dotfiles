a175735e77e6a9f9506e97a2b7a3e608e9a8dabd
dapaath <dafaathaullah123@gmail.com>
2022-12-20 08:10:02 +0700

    new-update Tue Dec 20 08:10:02 WIB 2022

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -124,6 +124,7 @@ lvim.builtin.treesitter.ensure_installed = {
   "java",
   "yaml",
   "fish",
+  "markdown",
 }
 
 lvim.builtin.treesitter.highlight.enabled = true
@@ -188,15 +189,6 @@ formatters.setup {
   { command = "black", filetypes = { "python" } },
   { command = "isort", filetypes = { "python" } },
   { command = "djhtml", filetypes = { "htmldjango" } },
-  {
-    -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-    command = "prettier",
-    ---@usage arguments to pass to the formatter
-    -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-    extra_args = { "--print-with", "100" },
-    ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-    filetypes = { "typescript", "typescriptreact" },
-  },
 }
 
 -- -- set additional linters
